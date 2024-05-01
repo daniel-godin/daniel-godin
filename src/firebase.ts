5ec22db012df1c84be3d1cffaea0752334e56939
Daniel Godin <DanielEGodin@gmail.com>
2024-05-01 10:37:24 -0700

    Firebase emulators set up.

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,6 +4,17 @@
 // Import the functions you need from the SDKs you need
 import { firebaseConfig } from "../firebase-config.ts";
 import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+
+
+
+// Firebase Emulators:
+connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+connectFirestoreEmulator(db, '127.0.0.1', 8080);
