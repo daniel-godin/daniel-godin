a086523b3e596326c1336f7adfdebf2a4f3c796a
Daniel Godin <DanielEGodin@gmail.com>
2024-05-01 10:27:31 -0700

    Firebase Init

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+// All Major Firebase Imports and Exports:
+
+// Imports:
+// Import the functions you need from the SDKs you need
+import { firebaseConfig } from "../firebase-config.ts";
+import { initializeApp } from "firebase/app";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
